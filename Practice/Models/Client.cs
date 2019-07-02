78bc974e78f75a82eeceaa190b1b7cfa46d85bda
Артём Заичкин <artyomsamara@yahoo.com>
2019-07-02 18:43:01 +0400

    Testing github, Rest API

---
+++ Practice/Models/Client.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practice.Models
+{
+    public class Client
+    {
+        public int ClientID
+        {
+            get;
+            set;
+        }
+        public string ClientFIO
+        {
+            get;
+            set;
+        }
+        public int ClientMobile
+        {
+            get;
+            set;
+        }
+        public string ClientCountry
+        {
+            get;
+            set;
+        }
+        public string ClientCity
+        {
+            get;
+            set;
+        }
+        public string ClientAdress
+        {
+            get;
+            set;
+        }
+    }
+}
